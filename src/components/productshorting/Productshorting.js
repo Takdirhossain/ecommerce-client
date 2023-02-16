8ea4bf4c3953f5e49c684a9e1eb2fbd4a5039560
Takdirhossain <takdirhossain35@gmail.com>
2023-02-16 15:27:24 +0600

    design shorting

--- src/components/productshorting/Productshorting.js
+++ src/components/productshorting/Productshorting.js
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import MenuIcon from '@mui/icons-material/Menu';
+import WidgetsIcon from '@mui/icons-material/Widgets';
+import "./productshorting.css"
 export default function Productshorting() {
   return (
-    <div>
-        
+    <div className='productshorrtingwrapper'>
+      <h2>All Product</h2>
+      <div className='shortingproduct'>
+        <input type="range" name="pricerange" id="" className='mr-5'/>
+        short by
+        <select>
+          <option value="Latestproduct">Latest product</option>
+          <option value="popularproduct">popular product</option>
+          <option value="popularproduct"> Price Low to High</option>
+          <option value="popularproduct"> price High to low</option>
+        </select>
+
+
+
+      </div>
+      <div>
+        view
+        <MenuIcon className='mr-5 ml-5'></MenuIcon>
+        <WidgetsIcon></WidgetsIcon>
+      </div>
+      <hr />
     </div>
   )
 }
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Productshorting from '../components/productshorting/Productshorting'
 
 export default function Product() {
   return (
     <div className='productmainpage w-[80%] m-auto'>
         <div>
-
+<Productshorting></Productshorting>
         </div>
         <div>
             
